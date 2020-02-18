44f10c1d72ebdff2654d26be11f9f72a7be063f5
Akadar.akku <akadar.akku121@gmail.com>
2020-02-18 21:59:06 +0100

    Updates

---
+++ precache-manifest.47ceb898d8aeb4ffed5df9fbce486be6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8242e5fd12687bed1f62a0434f03f4",
+    "url": "/pokedex-app/index.html"
+  },
+  {
+    "revision": "9f73d603531719d52afa",
+    "url": "/pokedex-app/static/css/main.e74e7625.chunk.css"
+  },
+  {
+    "revision": "c92b60f29cdf8f2b0920",
+    "url": "/pokedex-app/static/js/2.62f98365.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pokedex-app/static/js/2.62f98365.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f73d603531719d52afa",
+    "url": "/pokedex-app/static/js/main.69a9c0be.chunk.js"
+  },
+  {
+    "revision": "e4105332db56b43c8d86",
+    "url": "/pokedex-app/static/js/runtime-main.0eb14f00.js"
+  },
+  {
+    "revision": "6a398990b1d7b6c11135bd5823555f99",
+    "url": "/pokedex-app/static/media/sprites.6a398990.png"
+  }
+]);
